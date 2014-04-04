5a2620e90f4b50695a93c2cbfc348365c959c424
hekt <hektorg@gmail.com>
2014-04-04 21:58:07 +0900

    Separate XML functions

---
+++ Hooks/XmlSitemap.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+
+module Hooks.XmlSitemap (xmlSitemap) where
+
+import           Data.Text (Text, pack)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import           Data.Time.Clock (UTCTime)
+import           Database.MongoDB
+import           System.FilePath ((</>))
+
+import Model
+import IO
+import DB
+import XML
+
+
+dummyConf :: Configure
+dummyConf = Configure "note.hekt.org" "http://note.hekt.org/" "/Users/kaz/Works/blog-kari/tempaltes/article.html" "/Users/kaz/Works/blog-kari/html" "MyBlog" "127.0.0.1"
+
+
+xmlSitemap :: Configure -> [Article] -> IO ()
+xmlSitemap conf _ = do
+  e <- accessToBlog conf $ rest =<< find (select [] "articles")
+  case e of
+    Left  msg      -> putLog ErrorLog $ "XmlSitemap: " ++ show msg
+    Right articles -> generateSitemapFile conf $ map parseBSON articles
+
+generateSitemapFile :: Configure -> [Article] -> IO ()
+generateSitemapFile conf articles = 
+    let doc = generateSitemap conf articles
+        path = htmlDirectory conf </> "sitemap.xml"
+    in T.writeFile path $ renderXml doc
+
+generateSitemap :: Configure -> [Article] -> XmlDocument
+generateSitemap conf articles = XmlDocument "1.0" "UTF-8" $
+                                [elem (indexPage: map f articles)]
+    where 
+      indexPage = urlElem (blogUrl conf) (rfcTime $ blogLastMod articles)
+                  "weekly" "0.5"
+      f a = urlElem (article2url a) (rfcTime $ articleLastModified a) 
+            "never" "1.0"
+      elem ns = XmlElement "urlset" 
+                [("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9")]
+                (createXmlNodes ns)
+
+blogLastMod :: [Article] -> UTCTime
+blogLastMod articles = maximum $ map articleLastModified articles
+
+urlElem :: Text -> Text -> Text -> Text -> XmlElement
+urlElem loc mod freq pri = XmlElement "url" [] $ createXmlNodes
+                           [ simpleElem "loc" loc
+                           , simpleElem "lastmod" mod
+                           , simpleElem "changefreq" freq
+                           , simpleElem "priority" pri ]
+
+article2url :: Article -> Text
+article2url a = T.concat ["http://note.hekt.org/", tshow $ articleIdNum a]
+
+tshow :: Show a => a -> Text
+tshow = pack . show
+
+-- <url>
+-- <loc>http://www.example.com/</loc>
+-- <lastmod>2013-03-07</lastmod>
+-- <changefreq>daily</changefreq>
+-- <priority>1.0</priority>
+-- </url>
---
+++ XML.hs
@@ -0,0 +1,76 @@
+module XML
+    ( XmlDocument (XmlDocument)
+    , XmlElement (XmlElement)
+    , renderXml
+    , renderXml'
+    , cdata
+    , rfcTime
+    , simpleElem
+    , createTextNode
+    , createEmptyNode
+    , createXmlNodes
+    ) where
+
+
+import           Data.Text (Text, append, intercalate)
+import qualified Data.Text as T
+import           Data.Time.Clock
+import           Data.Time.Format
+import           System.Locale (defaultTimeLocale)
+
+
+data XmlDocument = XmlDocument { xmlVersion  :: Text
+                               , xmlEncoding :: Text
+                               , xmlElements :: [XmlElement] }
+data XmlElement = XmlElement { tagName    :: Text
+                             , attributes :: [(Text, Text)]
+                             , childNode  :: XmlContent }
+data XmlContent = EmptyNode | TextNode Text | XmlNodes [XmlElement] 
+
+
+renderXml :: XmlDocument -> Text
+renderXml doc = let ver = attr2text "version" $ xmlVersion doc
+                    enc = attr2text "encoding" $ xmlEncoding doc
+                in T.concat $ [ "<?xml ", ver, " ", enc, "?>" ]
+                       ++ map renderXml' (xmlElements doc)
+
+renderXml' :: XmlElement -> Text
+renderXml' (XmlElement tag attrs EmptyNode)     = renderEmptyTag tag attrs
+renderXml' (XmlElement tag attrs (TextNode t))  = renderTag tag attrs t
+renderXml' (XmlElement tag attrs (XmlNodes ns)) = renderTag tag attrs $ 
+                                                  T.concat $ map renderXml' ns
+
+renderTag :: Text -> [(Text, Text)] -> Text -> Text
+renderTag tag attrs content = 
+    let attrs' = attrs2texts attrs
+    in T.concat [ "<", intercalate " " (tag: attrs'), ">"
+                , content, "</", tag, ">" ]
+
+simpleElem :: Text -> Text -> XmlElement
+simpleElem tag content = XmlElement tag [] $ createTextNode content
+
+attr2text :: Text -> Text -> Text
+attr2text a v = T.concat [a, "=", "\"", v, "\""]
+
+attrs2texts :: [(Text, Text)] -> [Text]
+attrs2texts = map (uncurry attr2text)
+
+renderEmptyTag :: Text -> [(Text, Text)] -> Text
+renderEmptyTag tag attrs =
+    let attrs' = attrs2texts attrs
+    in T.concat ["<", intercalate " " (tag: attrs'), "/>"]
+
+cdata :: Text -> Text
+cdata d = T.concat ["<![CDATA[", d, "]]>"]
+
+rfcTime :: UTCTime -> Text
+rfcTime = T.pack . formatTime defaultTimeLocale "%FT%TZ"
+
+createEmptyNode :: XmlContent
+createEmptyNode = EmptyNode
+
+createTextNode :: Text -> XmlContent
+createTextNode = TextNode
+
+createXmlNodes :: [XmlElement] -> XmlContent
+createXmlNodes = XmlNodes
