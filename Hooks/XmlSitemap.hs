0bd79a85dd767ac940c27f236d61495e657f9978
hekt <hektorg@gmail.com>
2014-04-11 20:23:29 +0900

    Modify to use hastache template

--- Hooks/XmlSitemap.hs
+++ Hooks/XmlSitemap.hs
@@ -1,65 +1,61 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
 module Hooks.XmlSitemap (xmlSitemap) where
 
 import           Control.Monad.Error
+import           Data.Data (Data, Typeable)
 import qualified Data.Map as M
 import           Data.Maybe (catMaybes)
 import           Data.Text (Text, pack)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.IO as TL
 import           Data.Time.Clock (UTCTime)
+import           Data.Time.Format (formatTime)
+import           Text.Hastache
+import           Text.Hastache.Context
 import           Database.MongoDB
 import           System.FilePath ((</>))
+import           System.Locale (defaultTimeLocale)
 
 import Model
 import IO
 import DB
-import XML
+
+data BlogData     = BlogData { url     :: String
+                             , lastmod :: String
+                             } deriving (Data, Typeable)
+data TemplateData = TemplateData { blog     :: BlogData
+                                 , articles :: [TArticle]
+                                 } deriving (Data, Typeable)
 
 ioeLogger' = ioeLoggerWithLabel "XmlSitemap: "
 putLog' level = putLog level . (++) "XmlSitemap: "
 
 xmlSitemap :: Configure -> [Article] -> IO ()
 xmlSitemap conf _ = ioeLogger' . runErrorT $ do
-  docs <- ErrorT $ accessToBlog' conf $ 
-          rest =<< find (select [] "articles")
-          { project = ["id" =: 1, "last_modified" =: 1] }
-  let xml = generateSitemap conf $ map parseBSON docs
-      path = maybe (htmlDirectory conf </> "sitemap.xml") id $
-             "xml_sitemap_file" `M.lookup` optConfs conf
+  let tempPath   = templateDirectory conf </> "xml-sitemap.xml"
+      outputPath = htmlDirectory conf </> "sitemap.xml"
+  docs     <- ErrorT $ getDocuments conf
+  template <- liftIO $ decodeTemplateFile tempPath
+  res      <- liftIO $ buildContent conf $ map parseBSON docs
   liftIO $ do
-    T.writeFile path $ renderXml xml
-    putLog' InfoLog $ unwords ["Successfully generated", path]
-
-generateSitemap :: Configure -> [MaybeArticle] -> XmlDocument
-generateSitemap conf mas = XmlDocument "1.0" "UTF-8" $
-                           elem $ indexPage : catMaybes (map f mas)
-    where 
-      indexPage = urlElem (blogUrl conf) (rfcTime $ blogLastMod mas)
-                  "weekly" "0.5"
-      f ma = do
-        url <- fmap (id2url conf) $ mArticleIdNum ma
-        mod <- fmap rfcTime $ mArticleLastModified ma
-        return $ urlElem url mod "never" "1.0"
-      elem ns = [ XmlElement "urlset" 
-                  [("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9")]
-                  (createXmlNodes ns) ]
+    TL.writeFile outputPath res
+    putLog' InfoLog $ unwords ["Successfully generated", outputPath]
 
 blogLastMod :: [MaybeArticle] -> UTCTime
 blogLastMod articles = maximum . catMaybes $ map mArticleLastModified articles
 
-urlElem :: Text -> Text -> Text -> Text -> XmlElement
-urlElem loc mod freq pri = XmlElement "url" [] $ createXmlNodes
-                           [ simpleElem "loc" loc
-                           , simpleElem "lastmod" mod
-                           , simpleElem "changefreq" freq
-                           , simpleElem "priority" pri ]
+getDocuments :: Configure -> IO (Either String [Document])
+getDocuments conf = accessToBlog' conf $ rest =<< find (select [] "articles")
+                    { project = ["id" =: 1, "last_modified" =: 1] }
 
-id2url :: Configure -> Int -> Text
-id2url conf a = T.concat [blogUrl conf, tshow a]
-
-tshow :: Show a => a -> Text
-tshow = pack . show
-
+buildContent :: Configure -> [MaybeArticle] -> IO TL.Text
+buildContent conf marticles = do
+  template <- decodeTemplateFile $ templateDirectory conf </> "xml-sitemap.xml"
+  let blogData = BlogData (blogUrl conf) $ rfcTime $ blogLastMod marticles
+      tempData = TemplateData blogData $ map mArticleToTArticle marticles
+  hastacheStr defaultConfig template $ mkGenericContext tempData
